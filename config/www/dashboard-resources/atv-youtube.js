3bb2c86e5df08209d4c499e14b65e4c1a70a4112
Viet Ngoc <ngocjohn@me.com>
2024-02-17 03:32:08 +0100

    commit

---
+++ config/www/dashboard-resources/atv-youtube.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('atv_youtube_img');
+const hrefValue = element.getAttribute('data-url');
+
+if (hrefValue) {
+	// Use hrefValue here
+	console.log(hrefValue);
+}
